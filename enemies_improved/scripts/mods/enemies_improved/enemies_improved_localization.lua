6e1e61665c80a7ea62d747154ef96fa8aa67b14c
Alf Costello <atcostello1@gmail.com>
2026-09-12 16:10:54 +0100

    2.1.8
    - Added "remove tagged skulls" option (For Mr E).
    - Added ability to apply an opacity multiplier for the "global opacity" setting when you are aiming down sight. It lets you have the ADS opacity lower or higher than your regular opacity, which can be handy :)

--- enemies_improved/scripts/mods/enemies_improved/enemies_improved_localization.lua
+++ enemies_improved/scripts/mods/enemies_improved/enemies_improved_localization.lua
@@ -1,5 +1,5 @@
 local mod = get_mod("enemies_improved")
-mod.version = "2.1.7"
+mod.version = "2.1.8"
 mod:info("Enemies Improved is installed, using version: " .. tostring(mod.version))
 
 local next = next
